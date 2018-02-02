617d7ccd4add7fd66e26a3855171cf7647145b40
root <root@appwebcoopaductos.localdomain>
2018-02-02 01:22:35 +0000

    remote

--- public/index.php
+++ public/index.php
@@ -21,8 +21,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
 
+require __DIR__.'/../vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -35,7 +35,10 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
+
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
 
 /*
 |--------------------------------------------------------------------------
