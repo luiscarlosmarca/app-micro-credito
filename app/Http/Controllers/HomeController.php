72a26318edc569a8c06aa92b7e318105497077d4
luiscarlosmarca <luiscarlosmarca@gmail.com>
2018-03-08 19:50:40 -0500

    home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,8 +24,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index(){
 
        $y=date("Y");
        $m=date("m");
