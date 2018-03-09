7b1028e67c3f0a32af1e8ef13c4e046ca9d611eb
luiscarlosmarca <luiscarlosmarca@gmail.com>
2018-03-08 20:29:34 -0500

    zona horaria

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'EST',
 
     /*
     |--------------------------------------------------------------------------
@@ -181,6 +181,8 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         // ...
 
+        Yajra\DataTables\DataTablesServiceProvider::class,
+
         
     ],
 
@@ -235,6 +237,8 @@ return [
          'Form' => Collective\Html\FormFacade::class,
          'Html' => Collective\Html\HtmlFacade::class,
         // ...
+
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
         
 
     ],
